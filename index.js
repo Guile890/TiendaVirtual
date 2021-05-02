5fd4fafc7e50eccfef9e44861b3cacd5139e7cb0
BRCContreras <brandon.r.contreras@gmail.com>
2021-05-01 19:45:17 -0500

    mostrar articulos en demo2

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const urlCategoria = 'https://api.mercadolibre.com/sites/MLM/categories'
 const urlProducto = 'https://api.mercadolibre.com/sites/MLM/search?category='
+const listaMuestra = document.getElementById('listaMuestra')
+
+let arrayProductos = [];
+
+// class crearLista{
+//     constructor(Productos){
+
+
+//     }
+
+
+// }
+
 
 async function getCategoriasAPIMerca(){
     let respuesta = await fetch(urlCategoria)
@@ -8,11 +21,9 @@ async function getCategoriasAPIMerca(){
 }
 async function getInfoCategoria(){
     let resultado = await getCategoriasAPIMerca();
-    console.log(resultado)
-    
+    console.log(resultado)    
 }
 getInfoCategoria();
-
 
 
 async function getProductosByCategoria(id){
@@ -24,8 +35,41 @@ async function getProductosByCategoria(id){
 async function getInfoProductos(id){
     let resultado = await getProductosByCategoria(id);
     console.log(resultado);
-    
+     arrayProductos = resultado.results;
+      
+    mostrarProductos(arrayProductos); 
+    //  crearLista(arrayProductos);
 }
+
+
+
+function mostrarProductos(){
+    listaMuestra.innerHTML='';
+    
+        arrayProductos.forEach(element => {
+        console.log(element.title);
+        console.log(element.thumbnail);
+        listaMuestra.innerHTML += `
+        
+        <div class="col">
+          <div class="card shadow-sm">
+            <img src='${element.thumbnail}'>
+            <div class="card-body">
+                <p class="card-text"><b>${element.title}</b></p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                  </div>
+                  <small class="text-muted">9 mins</small>
+              </div>
+            </div>
+          </div>
+        `
+               });
+}
+
+
 getInfoProductos('MLM1132');
 
 getInfoProductos('MLM1403');
