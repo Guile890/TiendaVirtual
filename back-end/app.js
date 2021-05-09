28864fef24d9c2eb22d0ea01bdaed7dcc18b8647
Guile <guilabaldom@gmail.com>
2021-05-09 17:00:40 -0500

    generando swagger

--- back-end/app.js
+++ back-end/app.js
@@ -5,11 +5,14 @@ require("dotenv").config();
 const cors = require('cors');
 const midd = require('./midd/midd');
 const serviciosMercadoLibre = require("./services/services");
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./swagger_output.json')
 
 //middleware globales
 app.use(express.json());
 app.use(midd.limiter);
 app.use(cors());
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 //iniciar nuestro servidor
 
