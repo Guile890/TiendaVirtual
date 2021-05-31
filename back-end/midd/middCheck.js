db2228e5b0a66fa5999fabff1bb5402070081935
BRCContreras <brandon.r.contreras@gmail.com>
2021-05-31 16:32:51 -0500

    header y footer, correccion de joi

--- back-end/midd/middCheck.js
+++ back-end/midd/middCheck.js
@@ -3,7 +3,7 @@ module.exports = {
 
     modeloLogin : Joi.object().keys({
         email:Joi.string().email().required(),
-        pass: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+        contrasena: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
     }).with('email', 'contrasena')
 
 }
