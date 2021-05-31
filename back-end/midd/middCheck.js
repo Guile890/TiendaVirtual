28eb3e6572c566b664d1eb779a1440343617e6bb
BRCContreras <brandon.r.contreras@gmail.com>
2021-05-31 16:23:02 -0500

    joi en login

---
+++ back-end/midd/middCheck.js
@@ -0,0 +1,9 @@
+const Joi = require('@hapi/joi')
+module.exports = {
+
+    modeloLogin : Joi.object().keys({
+        email:Joi.string().email().required(),
+        pass: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required()
+    }).with('email', 'contrasena')
+
+}
